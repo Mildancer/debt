656a24d243f5e2a33b81ebde07181eb2e63f5782
milosz nawrat <milosznawrat@gmail.com>
2019-03-09 11:29:07 +0100

    debt

---
+++ src/main/java/pl/alx/debt/controller/HomeController.java
@@ -0,0 +1,16 @@
+package pl.alx.debt.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";                  // home to nazwa naszej strony
+    }
+
+
+}
